09587b5c1679d5499db87f8a4b4c58609fa1f82e
Rakhat Zhumekin <190103441@stu.sdu.edu.kz>
2021-04-12 17:01:18 +0600

    Some adjustments to the profile and posts index pages

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -3,10 +3,29 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('profile/profile.css') }}">
+    <script src="{{ asset('profile/profile.js') }}"></script>
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Profiles</title>
 </head>
 <body>
-    {{ $profiles }}
+    <div class="header">
+        <div id="add_profile">Add Profile</div>
+        <div id="add_post">Add Post</div>
+        <div id="posts">All Posts</div>
+    </div>
+    <hr size="2px" width="90%" color="#210b2c">
+    
+    @forelse ($profiles as $profile)
+        <table class="profiles">
+            <tr class="profile_row">
+                <td>{{$profile->id}}</td>
+                <td>{{$profile->name}}</td>
+                <td><img src="{{$profile->photo}}" width="100px"></td>
+            </tr>
+        </table>
+    @empty
+        <p id="nothing">There are no profiles registered.</p>
+    @endforelse
 </body>
 </html>
